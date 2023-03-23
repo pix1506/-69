1af82cb964794c5cea7bd355bcd1770942c9913d
pix1506 <128706897+pix1506@users.noreply.github.com>
2023-03-23 20:51:13 +0800

    Add files via upload

---
+++ Ex01.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+name='Bill'
+age=18
+rate=0.01
+check=True
+print(name)
+print(age)
+print(rate)
+print(check)
+print(100)
+print('LCC')
+
+print(type(name))
+print(type(age))
+print(type(rate))
+print(type(check))
+print(type(100))
+print(type('LCC'))
+
+#註解
+#多行註解
+''' 
+'''
+"""
+"""
+
